ce9ba0ff1ba281cfd7f76315091c0388a136e4f8
Mun-Seong <0611s@naver.com>
2022-02-22 19:01:38 +0900

    ..ing add_oper

--- Level_2/add_operator.cpp
+++ Level_2/add_operator.cpp
@@ -1,49 +1,38 @@
 #include <iostream>
 using namespace std;
 
-int	cal(int *nums, int oper[4], int n)
+int	cal(int *nums, int oper[4], int i, int n, int pre_ans)
 {
-	int idx, ret;
-	ret = nums[n];
-	if (n == 0)
-		return (ret);
-	for (int i=0;i<n;i++)
+	int ret, pre, next;
+	next = pre_ans;
+	pre = nums[i];
+
+	cout << pre << ' ' << ' ' << next << endl;
+	switch (i)
 	{
-		ret = nums[n];
-		idx = i;
-		while (oper[idx] == 0)
-			idx++;
-		if (idx > 3)
-		{
-			cout << "error" << endl;
-			return (ret);
-		}
-		oper[idx]--;
-		cout << ret << ' ' << idx << endl;
-		switch (idx)
-		{
-		case 0:
-			ret = cal(nums, oper, n - 1) + ret;
-			break;
-		case 1:
-			ret = cal(nums, oper, n - 1) - ret;
-			break;
-		case 2:
-			ret = cal(nums, oper, n - 1) * ret;
-			break;
-		case 3:
-			if (ret < 0)
-				ret = (cal(nums, oper, n - 1) / (ret * -1)) * -1;
-			else
-				ret = cal(nums, oper, n - 1) / ret;
-			break;
-		default:
-			break;
-		}
-		oper[idx]++;
-		cout << "ret : " << ret << ' ';
+	case 0:
+		ret = pre + next;
+		break;
+	case 1:
+		ret = pre - next;
+		break;
+	case 2:
+		ret = pre * next;
+		break;
+	case 3:
+		if (pre < 0 && next > 0)
+			ret = -1 * ((-1 * pre) / next);
+		else if (pre > 0 && next < 0)
+			ret = -1 * (pre / (-1 * next));
+		else
+			ret = pre / next;
+		break;
+	default:
+		break;
 	}
-	return ret;
+	cal(nums, oper, i--, n - 1, ret);
+	cout << "ret : " << ret << ' ';
+	return (ret);
 }
 
 int main()
@@ -57,6 +46,6 @@ int main()
 	for (int i = 0; i < 4; i++)
 		cin >> oper[i];
 	
-	cout << cal(nums, oper, n - 1);
+	cout << cal(nums, oper, n-2, n - 1, nums[n-1]);
 	delete[] nums;
 }

934220686d0388af3f16f2476d094d6ccdbf540b
Mun-Seong <0611s@naver.com>
2022-02-26 19:34:41 +0900

    finish teaching

--- Level_2/multitap_scheduling.cpp
+++ Level_2/multitap_scheduling.cpp
@@ -4,10 +4,49 @@ using namespace std;
 int n,k,ans(0);
 int *nums;
 int mul[100];
+int prio[100];
 
-int scheduling()
+int check_priority()
 {
-	
+	int min_idx(0), min_val(101);
+	for (int i=0;i<n;i++)
+	{
+		if (prio[mul[i]] < min_val)
+		{
+			min_idx = i;
+			min_val = prio[mul[i]];
+		}
+	}
+	return (min_idx);
+}
+
+void scheduling()
+{
+	bool flag(false);
+	int c_idx(0);
+	for (int i=n;i<k;i++)
+	{
+		flag = false;
+		for (int j=0;j<n;j++)
+		{
+			if (nums[i] == mul[j])
+			{
+				flag = true;
+				break;
+			}
+		}
+		if (!flag)
+		{	
+			c_idx = check_priority();
+			mul[c_idx] = nums[i];
+			cout << "\nafter : ";
+			for (int i=0;i<n;i++)
+				cout << mul[i] << ' ';
+			cout << ": " << nums[i];
+			ans++;
+		}
+		prio[nums[i]]--;
+	}
 }
 
 int main()
@@ -16,5 +55,15 @@ int main()
 	nums = new int[k];
 	for (int i=0;i<k;i++)
 		cin >> nums[i];
+	for (int i=0;i<k;i++)
+		prio[nums[i]]++;
+	for (int i=0;i<n;i++)
+	{
+		mul[i] = nums[i];
+		prio[nums[i]]--;
+	}
+
+	scheduling();
+	cout << ans;
 	delete[] nums;
 }
--- Level_2/teaching.cpp
+++ Level_2/teaching.cpp
@@ -1,15 +1,84 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
-int n,k;
-string *words;
+int n,k,ans(0);
+bool alpha[26];
+vector <string> words;
+
+void	check_words()
+{
+	int res(0);
+	for (int i = 0; i < n; i++)
+	{
+		bool c = true;
+		for (int j = 0; j < words[i].length(); j++)
+		{
+			if (!alpha[words[i][j] - 'a'])
+			{
+				c = false;
+				break;
+			}
+		}
+		if (c)
+			res++;
+		
+	}
+	ans = max(ans, res);
+	return ;
+}
+
+void	teach_char(int n_char, int idx)
+{
+	if (k == idx)
+	{
+		check_words();
+		return ;
+	}
+	else
+	{
+		for (int i = n_char; i < 26; i++)
+		{
+			if (!alpha[i])
+			{
+				alpha[i] = true;
+				teach_char(i, idx + 1);
+				alpha[i] = false;
+			}
+		}
+	}
+}
+
+void	init_alpha()
+{
+	alpha['a' - 'a'] = true;
+	alpha['c' - 'a'] = true;
+	alpha['i' - 'a'] = true;
+	alpha['n' - 'a'] = true;
+	alpha['t' - 'a'] = true;
+	k -= 5;
+}
 
 int main()
 {
 	cin >> n >> k;
-	words = new string[n];
-	for (int i=0;i<n;i++)
-		cin >> words[i];
-	cout << words[0].substr(4, words[0].length()-8);
+
+	if (k < 5) { cout << 0; return (0);}
+	else if (k == 26) { cout << n; return (0);}
+
+	init_alpha();
+	for (int i = 0; i < n; i++)
+	{
+		string s; cin >> s;
+		s.erase(s.begin(), s.begin() + 4);
+		s.erase(s.end() - 4, s.end());
+		words.push_back(s);
+	}
+
+	teach_char(0, 0);
+
+	cout << ans;
+	return (0);
 }

3655aff7679db2d7f1363f9aa81d773078fcb23a
Munseong Seo <museo@c4r8s1.42seoul.kr>
2023-04-26 22:20:00 +0900

    Study DP_2

--- DP_2/combine_files.cpp
+++ DP_2/combine_files.cpp
@@ -2,11 +2,26 @@
 // 11066 파일 합치기
 
 #include <iostream>
-#include <algorithm>
+#include <cstring>
 
 int	t, k;
-int	dp[501];
-int	chap[500];
+int	cost[501];
+int	sum[501];
+int	dp[501][501];
+
+int	dp_f(int start, int end)
+{
+	if (dp[start][end] != 0x3f3f3f3f)
+		return (dp[start][end]);
+	if (start == end)
+		return (dp[start][end] = 0);
+	for (int mid=start;mid<end;++mid) {
+		int l = dp_f(start, mid);
+		int r = dp_f(mid+1, end);
+		dp[start][end] = std::min(dp[start][end], l + r);
+	}
+	return (dp[start][end] += sum[end] - sum[start-1]);
+}
 
 int	main()
 {
@@ -18,12 +33,13 @@ int	main()
 	for (int i=0;i<t;++i) {
 		// input
 		std::cin >> k;
-		for (int j=0;j<k;++j) {
-			std::cin >> chap[j];
+		for (int j=1;j<=k;++j) {
+			std::cin >> cost[j];
+			sum[j] = sum[j-1] + cost[j];
 		}
 		// solution
-	
-		std::cout << dp[k-1] << '\n';
+		memset(dp, 0x3f, sizeof(dp));
+		std::cout << dp_f(1, k) << '\n';
 	}
 
 	return (0);

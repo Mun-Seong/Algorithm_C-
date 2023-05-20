67f7328fa5c2d90a7b76026885109d4c24158b94
Mun-Seong <0611s@naver.com>
2023-05-20 14:23:06 +0900

    finish_stack_2

--- Stack_2/reuion_oasis.cpp
+++ Stack_2/reuion_oasis.cpp
@@ -4,7 +4,8 @@
 #include <iostream>
 #include <stack>
 
-int				n, ans;
+int				n;
+long long int	ans;
 int				h[500000];
 std::stack<std::pair<int,int>>	s;
 
@@ -23,16 +24,17 @@ int	main()
 			s.pop();
 		}
 		if (!s.empty() && h[i] == s.top().first) {
-			ans += s.top().second + 1;
-			auto tmp = s.top();
-			s.pop();
-			tmp.second++;
-			s.push(tmp);
+			ans += s.top().second;
+			s.top().second++;
+			if (s.size() != 1)
+				ans++;
 		}
-		else
+		else {
+			if (!s.empty())
+				ans++;
 			s.push({h[i], 1});
+		}
 	}
 	std::cout << ans << '\n';
-
 	return (0);
 }

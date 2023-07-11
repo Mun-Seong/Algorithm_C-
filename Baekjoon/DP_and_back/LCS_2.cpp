aabc5f5a09f36507754e7579e9fa077412b7da85
Mun-Seong <0611s@naver.com>
2023-07-11 17:05:29 +0900

    study_dp_and_back

--- Baekjoon/DP_and_back/LCS_2.cpp
+++ Baekjoon/DP_and_back/LCS_2.cpp
@@ -6,7 +6,9 @@
 #include <vector>
 #include <string>
 
-int	ans[1001][1001];
+int		ans[1001][1001];
+char	save[1001];
+
 
 int	main()
 {
@@ -15,7 +17,31 @@ int	main()
 	std::cin >> a >> b;
 
 	// solution
-	
+	for (int i=1;i<=a.length();++i)
+	{
+		for (int j=1;j<=b.length();++j)
+		{
+			if (a[i-1] == b[j-1])
+				ans[i][j] = ans[i-1][j-1] + 1;
+			else
+				ans[i][j] = std::max(ans[i-1][j], ans[i][j-1]);
+		}
+	}
+	int aa = a.length();
+	int bb = b.length();
+	while (aa != 0 && bb != 0) {
+		if (ans[aa][bb] == ans[aa-1][bb])
+			aa--;
+		else if (ans[aa][bb] == ans[aa][bb-1])
+			bb--;
+		else {
+			save[ans[aa][bb]-1] = a[aa-1];
+			aa--;
+			bb--;
+		}
+	}
+	std::cout << ans[a.length()][b.length()] << '\n'; 
+	std::cout << save << std::endl;
 
 	return (0);
 }
